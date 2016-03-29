0a2ca67d38ffa9ef4afac6b096be1e341f5091b8
Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>
2016-03-29 01:29:22 -0300

    Multiplicação de matrizes

---
+++ TccApp/View/MatrixMultiply.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TccApp
+{
+	public partial class MatrixMultiply : ContentPage
+	{
+
+		private const string TAG = "MatrixMultiply";
+
+		private int[,] randomMatrixA, randomMatrixB;
+
+		public MatrixMultiply ()
+		{
+			InitializeComponent ();
+		}
+
+		private void InitialLoad() {
+		}
+
+		private void FillRandomMatrix(int size) {
+			randomMatrixA = new int[size, size];
+			randomMatrixB = new int[size, size];
+			double randomNumber;
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					randomNumber = new Random ().NextDouble ();
+					randomMatrixA [i, j] = (int)(randomNumber * 100);
+					randomNumber = new Random ().NextDouble ();
+					randomMatrixB [i, j] = (int)(randomNumber * 100);
+				}
+			}
+		}
+
+	}
+}
+
